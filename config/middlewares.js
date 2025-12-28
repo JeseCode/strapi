a41e63e05fa960e64de78b4ada11a19c8fa4be72
Jorge Castillo <jj.jorgecastillo@gmail.com>
2025-12-27 22:35:36 -0500

    fix: resolver conflicto de tipos en relacion plan para habilitar APIs

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,16 +1,16 @@
 module.exports = ({ env }) => [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000', 'https://cuentasdp.com'],
+      origin: ['*'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
     },
   },
+  'strapi::logger',
+  'strapi::security',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
